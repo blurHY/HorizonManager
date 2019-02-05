ce5a9c734e43e7547d7deae063f04786b7059fba
blurHY <blurhy@outlook.com>
2019-02-05 17:26:04 +0800

    Create centos7.sh

---
+++ centos7.sh
@@ -0,0 +1,26 @@
+# This is a installation script to install full HorizonSpider
+yum update
+yum install yum-utils
+yum groupinstall development
+yum install epel-release
+yum install https://centos7.iuscommunity.org/ius-release.rpm
+yum install python36u
+yum install python36u-pip
+yum install python36u-devel
+yum install nodejs
+yum install npm
+npm i yarn -g
+mkdir Horizon
+cd Horizon
+git clone https://github.com/HelloZeroNet/ZeroNet
+cd ZeroNet
+pip3.6 install -r requirements.txt
+cd ..
+git clone https://github.com/blurHY/HorizonSpider
+cd HorizonSpider
+pip3.6 install -r requirements.txt
+cd ..
+git clone https://github.com/blurHY/HorizonPanel
+cd HorizonPanel
+yarn
+yarn build
