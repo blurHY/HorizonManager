6597263bbfe5128050be7ccc0ab7d4a0f7b3c92b
blurHY <blurhy@outlook.com>
2019-02-05 16:43:15 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from app import sio, app
 
 sio.run(
-    app, host="127.0.0.1", port=5000, debug=False
+    app, host="0.0.0.0", port=5000, debug=False
 )  # Reloader may be not compatible with gevent,so just disable it.
 
 # To Run:
